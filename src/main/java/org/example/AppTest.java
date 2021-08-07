6c0e18735973230d47c9b4789f2464d1ce2ac997
xuxin2009 <724056979@qq.com>
2021-08-07 15:48:44 +0800

    add AppTest file

---
+++ src/main/java/org/example/AppTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * @author : sean
+ * @version V1.0
+ * @Project: day27_Manven
+ * @Package org.example
+ * @date Date : 2021年08月07日 15:47
+ * @Description: TODO
+ */
+
+public class AppTest {
+
+    public void AppTest()
+    {
+
+    }
+}
